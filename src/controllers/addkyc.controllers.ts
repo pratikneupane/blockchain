7008bd6a5af3a612fe812872f12721fad3863526
anamkc <anamkhatri10@gmail.com>
2023-08-11 13:06:05 +0545

    fix added token on response on login

--- src/controllers/addkyc.controllers.ts
+++ src/controllers/addkyc.controllers.ts
@@ -1,17 +1,29 @@
 import { Request, Response } from "express";
 
-
-
 export const addKycController = async (req: Request, res: Response) => {
- 
-    const body = req.body;
-    const{address, citizenshipImage , dob , email, firstname, lastname, phonenumber, profilepic} = body
-    if(!firstname || !lastname || !dob || !email || !phonenumber || !address || !citizenshipImage || !profilepic ){
-        return res.json({ message: "fill the form" }).status(400);
-      } else {
-        res.json({ message: "success" }).status(200);
-      }
-
-    
-}
-
+  const body = req.body;
+  const {
+    address,
+    citizenshipImage,
+    dob,
+    email,
+    firstname,
+    lastname,
+    phonenumber,
+    profilepic,
+  } = body;
+  if (
+    !firstname ||
+    !lastname ||
+    !dob ||
+    !email ||
+    !phonenumber ||
+    !address ||
+    !citizenshipImage ||
+    !profilepic
+  ) {
+    return res.json({ message: "fill the form" }).status(400);
+  } else {
+    res.json({ message: "success" }).status(200);
+  }
+};
--- src/index.ts
+++ src/index.ts
@@ -12,9 +12,9 @@ import connectDB from "./utils/connectDb";
 const app = express();
 connectDB();
 const blockchain = new Blockchain();
-const pubsub = new PubSub(blockchain);
+// const pubsub = new PubSub(blockchain);
 
-setTimeout(() => pubsub.broadcastBlockchain(), 2000);
+// setTimeout(() => pubsub.broadcastBlockchain(), 2000);
 
 app.use(express.json());
 app.use(cors());
@@ -33,7 +33,7 @@ app.get("/api/blocks", (req: Request, res: Response) => {
 app.post("/api/mine", (req: Request, res: Response) => {
   const { data } = req.body;
   blockchain.addBlock(data);
-  pubsub.broadcastBlockchain();
+  // pubsub.broadcastBlockchain();
   // console.log("New block added");
   // console.log(blockchain.chain);
   res.json(blockchain.chain);
