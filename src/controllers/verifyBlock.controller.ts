7987246369f5fe7662f728318bfb73fe1f7e12b3
pratikneupane <pratikneupane1999@gmail.com>
2023-11-25 14:14:43 +0545

    feat: financial institution signup api

--- src/controllers/verifyBlock.controller.ts
+++ src/controllers/verifyBlock.controller.ts
@@ -4,26 +4,29 @@ import User from "../models/User.model";
 
 export const verifyBlock = async (req: Request, res: Response) => {
   const { id } = req.body;
-  console.log(id)
-  console.log(req.body)
-  let block;
-  if (id) {
-    try {
-      const user = await User.findById(id);
-      const userHash = user && user.hash;
-      if(userHash === ""){
-        return res.json({message: "error"}).status(404);
-      }
-      block = blockchain.findOneByHash(userHash!);
-    } catch (err) {
-      console.log(err);
-      res.json({ message: "error" }).status(500);
-    }
+
+  if (!id || typeof id !== 'string') {
+    return res.status(400).json({ message: "Invalid ID format" });
   }
-  if(block) {
-    block.data.verified = true;
-    res.status(200).json({ message: "verified" });
-  } else {
-    res.status(404).json({ message: "Block not found" });
+
+  try {
+    const user = await User.findById(id);
+    const userHash = user && user.hash;
+
+    if (!userHash) {
+      return res.status(404).json({ message: "User not found or hash is empty" });
+    }
+
+    const block = blockchain.findOneByHash(userHash);
+
+    if (block) {
+      block.data.verified = true;
+      res.status(200).json({ message: "Block verified" });
+    } else {
+      res.status(404).json({ message: "Block not found" });
+    }
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "Internal server error" });
   }
 };
